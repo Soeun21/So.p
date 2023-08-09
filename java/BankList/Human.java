723329dd83858da723133366c5e39827e3762d39
Soeun21 <131752096+Soeun21@users.noreply.github.com>
2023-08-09 14:57:48 +0900

    Add files via upload

---
+++ java/BankList/Human.java
@@ -0,0 +1,39 @@
+package BankList;
+
+public class Human {
+	// 은행에 방문한 손님
+	
+	private String name;
+	private int age;
+	
+	public Human() {
+		// TODO Auto-generated constructor stub
+	}
+	
+
+	public Human(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+
+}
